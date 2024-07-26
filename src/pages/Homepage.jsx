d44c560a1c77a475c16238c87fbc7c61d0a4d4e0
Аброр <150520203+RikSanchez30@users.noreply.github.com>
2024-07-27 00:04:08 +0500

    Update Homepage.jsx

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -4,8 +4,8 @@ import Menu from "../Notepad/Menu"
 const HomePage = () => {
 
   return (
-<div className='text-[#9fadbc] bg-[#1d2125] pt-32  flex flex-col h-screen'>
-  <div className="laptop:container px-5 flex laptop:flex-row flex-col-reverse items-start gap-14">
+<div className='text-[#9fadbc] bg-[#1d2125] pt-32  w-full flex flex-col h-[115vh] laptop:h-[100vh]'>
+  <div className="laptop:container px-5 flex laptop:flex-row flex-col-reverse w-full justify-center items-start gap-14">
    <Menu/>
    <Home/>
    </div>
